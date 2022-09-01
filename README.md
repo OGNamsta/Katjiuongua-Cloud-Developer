4210c2ef022cd91f40527626c972a25a34b3c35f
Tengeevandu Katjiuongua <64324719+OGNamsta@users.noreply.github.com>
2022-09-01 10:24:42 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# cloud-developer
+content for Udacity's cloud developer nanodegree.
+Tengeevandu M. Katjiuongua
+
---
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -0,0 +1,36 @@
+export const config = {
+
+  "dev": {
+    "username": process.env.POSTGRESS_USERNAME,
+    "password": process.env.POSTGRESS_PASSWORD,
+    "database": process.env.POSTGRESS_DATABASE,
+    "host": process.env.POSTGRESS_HOST,
+    "dialect": "postgres",
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
+    },
+  "jwt": {
+    "secret": process.env.JWT_SECRET_KEY
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+    /*
+"dev1": {
+    "username": "katjiuonguadev",
+    "password": "udagramkatjiuonguadev",
+    "database": "udagramkatjiuonguadev",
+    "host": "udagramkatjiuonguadev.c6tmyp3g9rua.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "TengeeUdacityLab",
+    "aws_media_bucket": "udagramkatjiuonguadev"
+    },
+ 
+    */
+}
